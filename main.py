e50c67acb35b0eed1f8fac742787d28881908515
Duran914 <jd91435@gmail.com>
2019-06-27 16:19:05 -0700

    url function & session function. Added more locators to click_btn function

--- main.py
+++ main.py
@@ -15,8 +15,22 @@ def navigate_url(url):
 
 # button click: accepts a list of css selectors
         # ['#btn', '#nextBtn']
-def click_btn(buttons):
-        [browser.find_element_by_css_selector(button).click() for button in buttons]
+def click_btn(buttons, locate_by):
+        allowed_locator = {"id", "name", "xpath", "classname", "css"}
+
+        if locate_by == "" or locate_by not in allowed_locator:
+                raise ValueError("Enter a valid element locator")
+        else:
+                if locate_by == "css":
+                        [browser.find_element_by_css_selector(button).click() for button in buttons]
+                elif locate_by == "id":
+                        [browser.find_element_by_id(button).click() for button in buttons]
+                elif locate_by == "classname":
+                        [browser.find_element_by_class_name(button).click() for button in buttons]
+                elif locate_by == "xpath":
+                        [browser.find_element_by_xpath(button).click() for button in buttons]
+                elif locate_by == "name":
+                        [browser.find_element_by_name(button).click() for button in buttons]
         
 # Input text: accepts an dict of css selector & input 
         # eg. {'#formFirstname': 'Johnny'}
@@ -38,6 +52,22 @@ def submit_click(selector="#usi_content .usi_submitbutton"):
 def launch_modal():
         browser.execute_script("setTimeout( () => { usi_js.display(); }, 5000);")
 
+# Appends string url parameters
+def enable_url(param):
+        page_url = ""
+        if '?' in browser.current_url:
+                page_url = browser.current_url + "&" + param
+        else:
+                page_url = browser.current_url + "?" + param
+        navigate_url(page_url)
+        print(page_url)
+
+# Retrieves session cookie
+def get_session_cookie():
+        if browser.get_cookie('usi_sess') is None:
+                print("Could not retrieve session cookie")
+        return "USI session :" + browser.get_cookie('usi_sess')
+
 # Shuts down driver 
 def shutdown():
         browser.quit()
@@ -46,7 +76,7 @@ def shutdown():
 def halt_execution(sec):
         sleep(sec)
 
-
-
-
+navigate_url('https://www.officefurniture2go.com/')
+click_btn(['#ctl00_mainPlaceHolder_hlHeroSecond', '#clearFilters a'], locate_by='css')
+enable_url("usi_enable=1")
 
