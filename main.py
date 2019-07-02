85bbc47a3fa5c12bc934b1303644a3bedd314dd6
Duran914 <jd91435@gmail.com>
2019-07-02 15:45:29 -0700

    locator logic & error logging

--- main.py
+++ main.py
@@ -8,6 +8,25 @@ from time import sleep
 from config import browser # populate config.py 
 
 
+# selector validation
+def check_selector(locate_by="css"):
+        # Allowed locators
+        allowed_locator = {"xpath", "css"}
+
+        if locate_by == "" or locate_by not in allowed_locator:
+                return False
+        else:
+                return True
+
+# Send error to console (will update to write to file)
+def log_error(err):
+        for err_type, message in err.items():
+                if err_type == "value":
+                        raise ValueError(message)
+                elif err_type == "type":
+                        raise TypeError(message)
+                elif err_type == "name":
+                        raise NameError(message)
 
 # Navigates to url: Accepts 1 string argmuent
 def navigate_url(url):
@@ -15,31 +34,38 @@ def navigate_url(url):
 
 # button click: accepts a list of css selectors
         # ['#btn', '#nextBtn']
-def click_btn(buttons, locate_by):
-        allowed_locator = {"id", "name", "xpath", "classname", "css"}
-
-        if locate_by == "" or locate_by not in allowed_locator:
-                raise ValueError("Enter a valid element locator")
-        else:
+def click_btn(buttons, locate_by="css"):
+        if check_selector(locate_by):
+                # if locate_by == "" or locate_by not in allowed_locator:
+                #         raise ValueError("Enter a valid element locator")
+                # else:
                 if locate_by == "css":
                         [browser.find_element_by_css_selector(button).click() for button in buttons]
-                elif locate_by == "id":
-                        [browser.find_element_by_id(button).click() for button in buttons]
-                elif locate_by == "classname":
-                        [browser.find_element_by_class_name(button).click() for button in buttons]
                 elif locate_by == "xpath":
                         [browser.find_element_by_xpath(button).click() for button in buttons]
-                elif locate_by == "name":
-                        [browser.find_element_by_name(button).click() for button in buttons]
+        elif check_selector(locate_by) == False:
+                log_error({"value": "Enter a valid element locator"})
         
+
 # Input text: accepts an dict of css selector & input 
         # eg. {'#formFirstname': 'Johnny'}
-def input_text(inputData):
-        [browser.find_element_by_css_selector(selector).send_keys(value) for selector, value in inputData.items()]
+def input_text(inputData, locate_by="css"):
+        # if locate_by == "" or locate_by not in allowed_locator:
+        #         raise ValueError("Enter a valid element locator")
+        # else:
+        if check_selector(locate_by):
+                if locate_by == "css":
+                        [browser.find_element_by_css_selector(selector).send_keys(value) for selector, value in inputData.items()]
+                elif locate_by == "xpath":
+                        [browser.find_element_by_xpath(selector).send_keys(value) for selector, value in inputData.items()]
+        elif check_selector(locate_by) == False:
+                log_error({"value": "Enter a valid element locator"})
+        # [browser.find_element_by_css_selector(selector).send_keys(value) for selector, value in inputData.items()]
+
 
 # Hover & button click: accepts a list of a visible element selector and non-visible selector 
         # ['#menuBar', '#dropDown a']
-def hover_click_btn(visibleElement, hiddenElement):
+def hover_click_btn(visibleElement, hiddenElement, locate_by="css"):
         visibleEl = browser.find_element_by_css_selector(visibleElement)
         hiddenEl = browser.find_element_by_css_selector(hiddenElement)
         ActionChains(browser).move_to_element(visibleEl).click(hiddenEl).perform()
@@ -77,6 +103,7 @@ def halt_execution(sec):
         sleep(sec)
 
 navigate_url('https://www.officefurniture2go.com/')
-click_btn(['#ctl00_mainPlaceHolder_hlHeroSecond', '#clearFilters a'], locate_by='css')
-enable_url("usi_enable=1")
+click_btn(['#ctl00_mainPlaceHolder_hlHeroSecond', '#clearFilters a'], locate_by="css")
+input_text({'#ctl00_ucHeader_tbSearchQuery': "Testing"}, locate_by="csdfs")
+
 
