9ef3a48cd294ef064869fb800a790a5f66f21159
Duran914 <jd91435@gmail.com>
2019-07-03 13:24:47 -0700

    changes

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from time import sleep
-from config import browser # populate config.py 
+from config import path_to_drivers # populate config.py 
+
+
+browser = webdriver.Chrome()
+
+# output campaign info and pass/fail. (will write to another file later)
+def test_info(company, campaign_type, site_id, headless=False):
+        # Headless desktop broswer
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--window-size=1920x1080")
+
+        if headless == True:
+                gloabl browser = webdriver.Chrome(path_to_drivers)
+        
+        # if headless == True:
+        #         global browser = 
+        results = company + " " + campaign_type + " site_id " + site_id + ": Running..." 
+        print(results)
 
 
 # selector validation
@@ -13,10 +32,10 @@ def check_selector(locate_by="css"):
         # Allowed locators
         allowed_locator = {"xpath", "css"}
 
-        if locate_by == "" or locate_by not in allowed_locator:
-                return False
+        if locate_by not in allowed_locator:
+                log_error({"value": "Enter a valid element locator"})
         else:
-                return True
+                pass
 
 # Send error to console (will update to write to file)
 def log_error(err):
@@ -35,31 +54,27 @@ def navigate_url(url):
 # button click: accepts a list of css selectors
         # ['#btn', '#nextBtn']
 def click_btn(buttons, locate_by="css"):
-        if check_selector(locate_by):
-                # if locate_by == "" or locate_by not in allowed_locator:
-                #         raise ValueError("Enter a valid element locator")
-                # else:
-                if locate_by == "css":
-                        [browser.find_element_by_css_selector(button).click() for button in buttons]
-                elif locate_by == "xpath":
-                        [browser.find_element_by_xpath(button).click() for button in buttons]
-        elif check_selector(locate_by) == False:
-                log_error({"value": "Enter a valid element locator"})
+        check_selector(locate_by)
+        # if check_selector(locate_by):
+        if locate_by == "css":
+                [browser.find_element_by_css_selector(button).click() for button in buttons]
+        elif locate_by == "xpath":
+                [browser.find_element_by_xpath(button).click() for button in buttons]
+        # elif check_selector(locate_by) == False:
+        #         log_error({"value": "Enter a valid element locator"})
         
 
 # Input text: accepts an dict of css selector & input 
         # eg. {'#formFirstname': 'Johnny'}
 def input_text(inputData, locate_by="css"):
-        # if locate_by == "" or locate_by not in allowed_locator:
-        #         raise ValueError("Enter a valid element locator")
-        # else:
-        if check_selector(locate_by):
-                if locate_by == "css":
-                        [browser.find_element_by_css_selector(selector).send_keys(value) for selector, value in inputData.items()]
-                elif locate_by == "xpath":
-                        [browser.find_element_by_xpath(selector).send_keys(value) for selector, value in inputData.items()]
-        elif check_selector(locate_by) == False:
-                log_error({"value": "Enter a valid element locator"})
+        # if check_selector(locate_by):
+        check_selector(locate_by)
+        if locate_by == "css":
+                [browser.find_element_by_css_selector(selector).send_keys(value) for selector, value in inputData.items()]
+        elif locate_by == "xpath":
+                [browser.find_element_by_xpath(selector).send_keys(value) for selector, value in inputData.items()]
+        # elif check_selector(locate_by) == False:
+        #         log_error({"value": "Enter a valid element locator"})
         # [browser.find_element_by_css_selector(selector).send_keys(value) for selector, value in inputData.items()]
 
 
@@ -70,23 +85,26 @@ def hover_click_btn(visibleElement, hiddenElement, locate_by="css"):
         hiddenEl = browser.find_element_by_css_selector(hiddenElement)
         ActionChains(browser).move_to_element(visibleEl).click(hiddenEl).perform()
 
+
 # Submit Button Click: Accepts css selector or default value will be used
 def submit_click(selector="#usi_content .usi_submitbutton"):
         WebDriverWait(browser, 90).until(EC.element_to_be_clickable((By.CSS_SELECTOR, selector))).click()
         
+
 # Launches Modal: No args accepted 
 def launch_modal():
         browser.execute_script("setTimeout( () => { usi_js.display(); }, 5000);")
 
+
 # Appends string url parameters
-def enable_url(param):
+def append_url(param):
         page_url = ""
         if '?' in browser.current_url:
                 page_url = browser.current_url + "&" + param
         else:
                 page_url = browser.current_url + "?" + param
         navigate_url(page_url)
-        print(page_url)
+
 
 # Retrieves session cookie
 def get_session_cookie():
@@ -96,14 +114,19 @@ def get_session_cookie():
 
 # Shuts down driver 
 def shutdown():
+        sleep(5)
         browser.quit()
+
 
 # Halts execution of script (last case scenario)
 def halt_execution(sec):
         sleep(sec)
 
+test_info("Office-Furniture-to-go", "TT", "24586", headless=True)
 navigate_url('https://www.officefurniture2go.com/')
 click_btn(['#ctl00_mainPlaceHolder_hlHeroSecond', '#clearFilters a'], locate_by="css")
-input_text({'#ctl00_ucHeader_tbSearchQuery': "Testing"}, locate_by="csdfs")
+append_url('usi_enable=1')
+input_text({"#ctl00_ucHeader_tbSearchQuery": "Testing"}, locate_by="css")
+shutdown()
 
 
