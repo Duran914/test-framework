998a4e4693203df17afdbfee35aadcd5ceeaf16a
Duran914 <jonathan.duran.100@my.csun.edu>
2019-07-05 19:13:06 -0700

    added mobile/headless mode & screenshot function

--- main.py
+++ main.py
@@ -6,24 +6,31 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from time import sleep
-from config import path_to_drivers # populate config.py 
+from config import driver_path # populate config.py 
 
 
-browser = webdriver.Chrome()
 
-# output campaign info and pass/fail. (will write to another file later)
-def test_info(company, campaign_type, site_id, headless=False):
-        # Headless desktop broswer
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--window-size=1920x1080")
+# output campaign info and pass/fail. (will log test to a txt later)
+def test_info(company, campaign_type, site_id, driver_path=driver_path, device_type="desktop", headless=False):
+        browser_options = Options()
+        global browser
 
+        # Mobile execution
+        if device_type == "mobile":
+                mobile_emulation = { "deviceName": "iPhone X" } # Iphone X for now
+                browser_options.add_experimental_option("mobileEmulation", mobile_emulation)
+        else:
+                browser_options.add_argument("--window-size=1920x1080") # Desktop execution
+
+        # Runs broswer in headless mode
         if headless == True:
-                gloabl browser = webdriver.Chrome(path_to_drivers)
+                browser_options.add_argument("--headless")
+                browser = webdriver.Chrome(driver_path, chrome_options=browser_options)
+        else:
+                browser = webdriver.Chrome(driver_path, chrome_options=browser_options) 
         
-        # if headless == True:
-        #         global browser = 
-        results = company + " " + campaign_type + " site_id " + site_id + ": Running..." 
+        # Log script header
+        results = f"{company} {campaign_type} {site_id}: Running..." 
         print(results)
 
 
@@ -37,6 +44,7 @@ def check_selector(locate_by="css"):
         else:
                 pass
 
+
 # Send error to console (will update to write to file)
 def log_error(err):
         for err_type, message in err.items():
@@ -47,40 +55,36 @@ def log_error(err):
                 elif err_type == "name":
                         raise NameError(message)
 
+
 # Navigates to url: Accepts 1 string argmuent
 def navigate_url(url):
         browser.get(url)
+
 
 # button click: accepts a list of css selectors
         # ['#btn', '#nextBtn']
 def click_btn(buttons, locate_by="css"):
         check_selector(locate_by)
-        # if check_selector(locate_by):
         if locate_by == "css":
                 [browser.find_element_by_css_selector(button).click() for button in buttons]
         elif locate_by == "xpath":
                 [browser.find_element_by_xpath(button).click() for button in buttons]
-        # elif check_selector(locate_by) == False:
-        #         log_error({"value": "Enter a valid element locator"})
         
 
 # Input text: accepts an dict of css selector & input 
         # eg. {'#formFirstname': 'Johnny'}
 def input_text(inputData, locate_by="css"):
-        # if check_selector(locate_by):
         check_selector(locate_by)
         if locate_by == "css":
                 [browser.find_element_by_css_selector(selector).send_keys(value) for selector, value in inputData.items()]
         elif locate_by == "xpath":
                 [browser.find_element_by_xpath(selector).send_keys(value) for selector, value in inputData.items()]
-        # elif check_selector(locate_by) == False:
-        #         log_error({"value": "Enter a valid element locator"})
-        # [browser.find_element_by_css_selector(selector).send_keys(value) for selector, value in inputData.items()]
 
 
 # Hover & button click: accepts a list of a visible element selector and non-visible selector 
         # ['#menuBar', '#dropDown a']
 def hover_click_btn(visibleElement, hiddenElement, locate_by="css"):
+        check_selector(locate_by)
         visibleEl = browser.find_element_by_css_selector(visibleElement)
         hiddenEl = browser.find_element_by_css_selector(hiddenElement)
         ActionChains(browser).move_to_element(visibleEl).click(hiddenEl).perform()
@@ -112,21 +116,29 @@ def get_session_cookie():
                 print("Could not retrieve session cookie")
         return "USI session :" + browser.get_cookie('usi_sess')
 
+
 # Shuts down driver 
 def shutdown():
         sleep(5)
         browser.quit()
+        print("Test complete")
 
 
 # Halts execution of script (last case scenario)
 def halt_execution(sec):
         sleep(sec)
 
-test_info("Office-Furniture-to-go", "TT", "24586", headless=True)
+
+def take_screenshot(screenshot_name="default.png"):
+        browser.save_screenshot(f"{screenshot_name}.png")
+
+
+test_info("Office-Furniture-to-go", "TT", "24586", driver_path=driver_path, device_type="desktop")
 navigate_url('https://www.officefurniture2go.com/')
 click_btn(['#ctl00_mainPlaceHolder_hlHeroSecond', '#clearFilters a'], locate_by="css")
 append_url('usi_enable=1')
 input_text({"#ctl00_ucHeader_tbSearchQuery": "Testing"}, locate_by="css")
+take_screenshot('input_test')
 shutdown()
 
 
